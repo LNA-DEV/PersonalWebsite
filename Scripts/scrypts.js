b8f54e52567d9f67745845adfa6bf901dd6aa5ae
Lukas Nagel <nagellukas04@outlook.com>
2021-05-27 12:23:30 +0200

    Fixed #3

--- Scripts/scrypts.js
+++ Scripts/scrypts.js
@@ -13,29 +13,4 @@ function ScrollToBottom()
     window.scrollTo({top: 999999 , behavior: 'smooth'});
 }
 
-window.onload=function(){
-    SetText();
-}
-
-function SetText()
-{    
-    var para = document.getElementById("value");
-    var output = window.location.href;
-    output = output.substring(output.indexOf("?")+1);
-    output = output.replace("&","\n");
-    para.innerText = output;
-}
-
-function DropDown()
-{
-    var root = document.getElementById('myDropdown');
-    if(!root.classList.contains("dropdown-contentE"))
-    {
-        root.classList.add("dropdown-contentE");
-    }
-    else
-    {
-        root.classList.remove("dropdown-contentE");
-    }
-}
 
